5265baa9b69dfaa8ebb16a704c94d0d2e76503bc
Thomas Li <tli186@ucr.edu>
2023-12-02 20:56:16 -0800

    add STL sort for now
    Performance: 20.5s

--- semisort.h
+++ semisort.h
@@ -3,7 +3,13 @@
 
 #include "parlaylib/include/parlay/parallel.h"
 
+// hash function
+template <class T>
+T h(T x) {
+    return x;
+}
+
 template <class T>
 void semisort(T *A, size_t n) {
-    
+    std::sort(A, A+n);
 }

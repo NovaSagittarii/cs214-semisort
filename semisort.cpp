4adf9a981c296f6e3cd832165dbb7c617e13c134
Thomas Li <tli186@ucr.edu>
2023-12-05 22:19:01 -0800

    add test framework to support multiple parameters

--- semisort.cpp
+++ semisort.cpp
@@ -13,19 +13,12 @@
 
 using Type = std::pair<long long, long long>;
 
-int main(int argc, char* argv[]) {
-  size_t n = 1e9;
-  int num_rounds = 3;
-  int random_seed = 1;
-  if (argc >= 2) n = atoll(argv[1]);
-  if (argc >= 3) num_rounds = atoi(argv[2]);
-  if (argc >= 4) random_seed = atoi(argv[3]);  
-
-  const size_t element_types = 10000000;
+void run_test(size_t n, size_t keys, int num_rounds, int random_seed) {
   Type* A = (Type*)malloc(n * sizeof(Type));
-  parlay::parallel_for(0, n, [&](size_t j) { A[j] = {hash64(j * random_seed) % element_types, hash64(j * random_seed)}; });
+  parlay::parallel_for(0, n, [&](size_t j) { A[j] = {hash64(j * random_seed) % keys, hash64(j * random_seed)}; });
+  
   // Note: checker doesn't run well with few duplicates.
-  const bool run_checker = n <= 1e7 || element_types <= 1e4;
+  const bool run_checker = false && (n <= 1e7 || keys <= 1e4);
   // [Checker] get counts
   std::map<long long, int> counts;
   if (run_checker) {
@@ -38,6 +31,7 @@ int main(int argc, char* argv[]) {
   double total_time = 0;
   for (int i = 0; i <= num_rounds; i++) {
     // Generate random arrays
+    parlay::parallel_for(0, n, [&](size_t j) { A[j] = {hash64(j * random_seed) % keys, hash64(j * random_seed)}; });
 
     parlay::internal::timer t;
     semisort(A, n);
@@ -72,5 +66,21 @@ int main(int argc, char* argv[]) {
     }
   }
   std::cout << "Average running time: " << total_time / num_rounds << std::endl;
+}
+
+int main(int argc, char* argv[]) {
+  size_t n = 1e9;
+  int num_rounds = 3;
+  int random_seed = 1;
+  if (argc >= 2) n = atoll(argv[1]);
+  if (argc >= 3) num_rounds = atoi(argv[2]);
+  if (argc >= 4) random_seed = atoi(argv[3]);  
+
+  for (auto element_types : {1e1, 1e3, 1e5, 1e7, 1e9}) {
+    std::cout << "Keys=" << element_types << std::endl;
+    run_test(n, element_types, num_rounds, random_seed);
+    std::cout << std::endl;
+  }
+  
   return 0;
 }

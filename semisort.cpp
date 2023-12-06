da91b90976c4050df125206a5249771ff9c0b79b
Thomas Li <tli186@ucr.edu>
2023-12-05 21:36:13 -0800

    selectively run the checker (when it doesn't take too long)

--- semisort.cpp
+++ semisort.cpp
@@ -4,13 +4,14 @@
 #include <cstdlib>
 #include <iostream>
 #include <map>
+#include <utility>
 
 #include "parlaylib/include/parlay/parallel.h"
 #include "parlaylib/include/parlay/internal/get_time.h"
 
 #include "hash64.h"
 
-using Type = long long;
+using Type = std::pair<long long, long long>;
 
 int main(int argc, char* argv[]) {
   size_t n = 1e9;
@@ -20,14 +21,18 @@ int main(int argc, char* argv[]) {
   if (argc >= 3) num_rounds = atoi(argv[2]);
   if (argc >= 4) random_seed = atoi(argv[3]);  
 
+  const size_t element_types = 10000000;
   Type* A = (Type*)malloc(n * sizeof(Type));
-  parlay::parallel_for(0, n, [&](size_t j) { A[j] = hash64(j * random_seed) % 1000; });
+  parlay::parallel_for(0, n, [&](size_t j) { A[j] = {hash64(j * random_seed) % element_types, hash64(j * random_seed)}; });
   // Note: checker doesn't run well with few duplicates.
-
+  const bool run_checker = n <= 1e7 || element_types <= 1e4;
   // [Checker] get counts
-  std::map<int, int> counts;
-  for (size_t i = 0; i < n; ++i) {
-    ++counts[A[i]];
+  std::map<long long, int> counts;
+  if (run_checker) {
+    std::cout << "checker is enabled" << std::endl;
+    for (size_t i = 0; i < n; ++i) {
+      ++counts[A[i].first];
+    }
   }
 
   double total_time = 0;
@@ -39,23 +44,24 @@ int main(int argc, char* argv[]) {
     t.stop();
 
     // [Checker] check counts and semisorted (when it changes, there should be no more left)
-    int curr = -1;
-    int remaining = 0;
-    for (size_t i = 0; i < n; ++i) {
-      const auto x = A[i];
-      if (x != curr) {
-        if (remaining) {
-          std::cout << "Output is not semisorted\n";
-          std::cout << "Segment with key=" << curr << " ended with " << remaining << " missing elements\n";
-          std::cout << "Found " << (counts[x]-remaining) << " elements but expected " << counts[x] << " elements\n";
-          exit(0);
+    if (run_checker) {
+      long long curr = -1;
+      int remaining = 0;
+      for (size_t i = 0; i < n; ++i) {
+        const auto x = A[i].first;
+        if (x != curr) {
+          if (remaining) {
+            std::cout << "Output is not semisorted\n";
+            std::cout << "Segment with key=" << curr << " ended with " << remaining << " missing elements\n";
+            std::cout << "Found " << (counts[x]-remaining) << " elements but expected " << counts[x] << " elements\n";
+            exit(0);
+          }
+          curr = x;
+          remaining = counts[x];
         }
-        curr = x;
-        remaining = counts[x];
+        --remaining;
       }
-      --remaining;
     }
-
 
     if (i == 0) {
       std::cout << "Warmup round running time: " << t.total_time() << std::endl;
